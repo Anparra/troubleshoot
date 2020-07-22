5916e830be664cfeb109bdd224d80e09e0eb9a9c
Anparra <47276601+Anparra@users.noreply.github.com>
2020-07-22 16:30:37 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
     makerController.player1.setAnalog(ArcadeAnalogButton.DownUp, input.acceleration(Dimension.Y))
-    if (true) {
-    	
+    if (input.acceleration(Dimension.X) >= 500) {
+        light.showRing(
+        `black black black black black black red red red black`
+        )
+    } else if (input.acceleration(Dimension.X) < -500) {
+        light.showRing(
+        `black red red red black black black black black black`
+        )
+    } else if (input.acceleration(Dimension.Y) >= 500) {
+        light.showRing(
+        `red black black black black black black black black red`
+        )
+    } else if (input.acceleration(Dimension.Y) < -500) {
+        light.showRing(
+        `black black black black red red black black black black`
+        )
+    } else {
+        light.showRing(
+        `black black black black black black black black black black`
+        )
     }
 })
