b4ae211003563e0d90120144c3bc4999241879d3
Anparra <47276601+Anparra@users.noreply.github.com>
2020-07-22 16:26:16 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+forever(function () {
+    makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
+    makerController.player1.setAnalog(ArcadeAnalogButton.DownUp, input.acceleration(Dimension.Y))
+    if (true) {
+    	
+    }
+})
